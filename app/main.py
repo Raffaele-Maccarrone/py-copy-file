caa10cee7cc29c735ae1d9c2f75be3dbb76e0f9f
Raffaele Maccarrone <raffaele.alves.maccarrone@gmail.com>
2026-05-02 17:35:23 -0300

    Solução

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    files = command.split(" ")
+    if len(files) != 3 or files[0] != "cp":
+        return
+    file1 = files[1]
+    file2 = files[2]
+    if file1 != file2:
+        try:
+            with open(file1, "r") as f:
+                content = f.read()
+            with open(file2, "a") as g:
+                g.write(content)
+        except FileNotFoundError:
+            pass
